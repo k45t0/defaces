56db26586542a56393ff8ff0bf46da76dd43d4ad
Wh0l5Th3R00t <155916762+k45t0@users.noreply.github.com>
2024-05-15 16:36:03 -0300

    Update root.js

--- root.js
+++ root.js
@@ -1,1 +1,93 @@
-a
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hacked By Root Society</title>
+    <link rel="shortcut icon" href="https://d.img.vision/2015/root.png" type="image/x-icon">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap');
+
+        * {
+            cursor: url(http://cur.cursors-4u.net/symbols/sym-1/sym46.cur), progress !important;
+            font-family: "Libre Baskerville", serif;
+        }
+
+        body {
+            background-color: black;
+            color: white;
+            padding: 10px;
+        }   
+
+        img {
+            width: 20%;
+            filter: drop-shadow(0 0 90px rgba(151, 151, 151, 0.171));
+            border-radius: 50%;
+        }
+
+        h1 {
+            padding: 10px;
+            font-size: 36px;
+            color: red;
+            filter: drop-shadow(0 0 10px rgb(255, 0, 0));
+        }
+
+        h2 {
+            width: 15%;
+            padding: 5px;
+            margin: 30x;
+            font-family: sans-serif;
+            filter: drop-shadow(0 0 5px rgba(182, 182, 182, 0.74));
+            top: 0;
+            right: 0;
+            color: white;
+            border-radius: 7px;
+            transition: .2s;
+        }
+
+        h2:hover {
+            transition: .2s;
+            color: rgb(223, 223, 223);
+            filter: drop-shadow(0 0 7px rgb(255, 0, 0));
+        }
+
+        p {
+            font-size: 20px;
+
+        }
+
+        #i {
+            filter: drop-shadow(0 0 10px rgb(0, 255, 0));
+            color: rgb(72, 255, 0);
+        }
+        
+        u {
+            font-size: 20px;
+            text-decoration-color: rgb(255, 0, 0);
+            text-decoration-thickness: 3px;
+            filter: drop-shadow(0 0 5px rgb(255, 0, 0));
+        }
+
+        a {
+            width: 0%;
+            text-decoration: none;
+        }
+
+    </style>
+</head>
+<center>
+<body>
+    <img src="https://d.img.vision/2015/root.png" alt="OpLockBit">
+    <h1>Hacked By Root Society</h1>
+    <span id="a"><p>Estamos realizando esta invasão como um grito de protesto contra as injustiças do atual governo brasileiro. Esta é apenas a primeira de muitas ações que virão, uma tempestade de indignação que não pode mais ser contida. Não se trata de uma questão pessoal, mas sim da defesa dos valores democráticos e da transparência que nosso país merece.
+        Não podemos mais fechar os olhos para o que está acontecendo em nosso país. <u>Obtivemos acesso a uma série de documentos confidenciais do governo brasileiro que revelam segredos de um sistema corroído pelo poder e pela corrupção. E em breve, compartilharemos essas verdades com o mundo.</u>
+            O atual presidente do Brasil, Luiz Inácio Lula da Silva, pode pensar que está acima da lei, que seus segredos estão seguros. Mas estamos aqui para provar o contrário. Chegou a hora de expor a verdade, de tirar o véu que encobre as ações nefastas daqueles que deveriam nos representar.
+            Esta é nossa mensagem final: os dias de impunidade estão contados. O povo brasileiro clama por justiça, e nós seremos sua voz. Estamos prontos para enfrentar as consequências, pois acreditamos que é nosso dever lutar por um Brasil melhor, mais justo e transparente para todos.</p>
+    <br>
+            <p id="i">Be the root. Open your mind. Open your eyes.</p>
+            <a href="https://t.me/R00tSociety">
+                <h2>TELEGRAM GROUP</h2>
+            </a>            
+</body>
+</center>
+</html>
